65da7a8330f3cf775f0cf4dfdb9899a166351ce6
pg132 <42481851+pg132@users.noreply.github.com>
2018-11-08 18:07:21 -0800

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,58 @@ var user = {
     mk1:{
         cost:10,
         amount:0,
-    }
+        multiplier:1,
+        base:0
+    },
+    mk2:{
+        cost:100,
+        amount:0,
+        multiplier:1,
+        base:0
+    },
+    mk3:{
+        cost:10,
+        amount:0,
+        multiplier:1,
+        base:0
+    },
+    mk4:{
+        cost:10,
+        amount:0,
+        multiplier:1,
+        base:0
+    },
+    mk5:{
+        cost:10,
+        amount:0,
+        multiplier:1,
+        base:0
+    },
+    mk6:{
+        cost:10,
+        amount:0,
+        multiplier:1,
+        base:0
+    },
+    mk7:{
+        cost:10,
+        amount:0,
+        multiplier:1,
+        base:0
+    },
+    mk8:{
+        cost:10,
+        amount:0,
+        multiplier:1,
+        base:0
+    },
+    mk9:{
+        cost:10,
+        amount:0,
+        multiplier:1,
+        base:0
+    },
+    
 
 
 
