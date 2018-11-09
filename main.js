6361144b20c55f4e998fc491d2fb968e785170aa
pg132 <42481851+pg132@users.noreply.github.com>
2018-11-08 18:14:23 -0800

    Update main.js

--- main.js
+++ main.js
@@ -7,59 +7,69 @@ var user = {
         cost:10,
         amount:0,
         multiplier:1,
-        base:0
+        base:0,
+        previousTierCost:0
     },
     mk2:{
         cost:100,
         amount:0,
         multiplier:1,
-        base:0
+        base:0,
+        previousTierCost:10
     },
     mk3:{
         cost:10,
         amount:0,
         multiplier:1,
-        base:0
+        base:0,
+        previousTierCost:10
     },
     mk4:{
         cost:10,
         amount:0,
         multiplier:1,
-        base:0
+        base:0,
+        previousTierCost:10
     },
     mk5:{
         cost:10,
         amount:0,
         multiplier:1,
-        base:0
+        base:0,
+        previousTierCost:10,
+        previousTierCost:10
     },
     mk6:{
         cost:10,
         amount:0,
         multiplier:1,
         base:0,
-        unlocked:false
+        unlocked:false,
+        previousTierCost:10
     },
     mk7:{
         cost:10,
         amount:0,
         multiplier:1,
         base:0,
-        unlocked:false
+        unlocked:false,
+        previousTierCost:10
     },
     mk8:{
         cost:10,
         amount:0,
         multiplier:1,
         base:0,
-        unlocked:false
+        unlocked:false,
+        previousTierCost:10
     },
     mk9:{
         cost:10,
         amount:0,
         multiplier:1,
         base:0,
-        unlocked:false
+        unlocked:false,
+        previousTierCost:10
     },
     wells:{
         cost:20,
