9af167e5c7c7b19b92da0125417203692f222c0e
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-11-10 20:20:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -125,7 +125,7 @@ function buyMK(tier) {
 			user.mk1.amount = user.mk1.amount.plus(1)
 			user.mk1.base += 1
 		}
-	}else if (gravCost.lte(user.gravicles) && tierCost.lte(user["mk"+(tier-1)].amount)&&tier<=5&&tier>=2){
+	}else if (gravCost.lte(user.gravicles) && user["mk"+(tier-1)].amount.lte(tierCost)&&tier<=5&&tier>=2){
 		user.gravicles = user.gravicles.minus(gravCost)
 		user["mk"+tier].cost = user["mk"+tier].cost.times(costMult)
 		//what should the multiplier formula be? rn im gonna make it 1% stronger
@@ -133,7 +133,7 @@ function buyMK(tier) {
 		user["mk"+(tier-1)].amount = user["mk"+(tier-1)].amount.minus(tierCost)
 		user["mk"+tier].amount = user.mk1.amount.plus(1)
 		user["mk"+tier].base += 1
-	} else if (gravCost.lte(user.gravicles) && tierCost.lte(user["mk"+(tier-1)].amount)&&tier>=6){
+	} else if (gravCost.lte(user.gravicles) && user["mk"+(tier-1)].amount.lte(tierCost)&&tier>=6){
 		if (user["mk"+tier].unlocked == true){
 			user.gravicles = user.gravicles.minus(gravCost)
 			user["mk"+tier].cost = user["mk"+tier].cost.times(costMult)
