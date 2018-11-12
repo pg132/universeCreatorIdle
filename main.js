021279f29c45f52e386c5bcf5971243238295f68
pg132 <42481851+pg132@users.noreply.github.com>
2018-11-11 17:34:14 -0800

    Update main.js

--- main.js
+++ main.js
@@ -127,7 +127,7 @@ function buyMK(tier) {
 			user.mk1.base += 1
 			if(user.mk1.base > 30 && user.mk1.base % 10 === 0) {
 				user.mk1.costMult *= 1.01;
-				if (user.mk1.base%50 === 0) user.mk1.costMult *= 1.02
+				if (user.mk1.base%50 === 0 && user.mk1.base > 300) user.mk1.costMult *= 1.02
 			}
 		}
 	} else if (gravCost.lte(user.gravicles) && user["mk"+(tier-1)].amount.gte(tierCost)&&tier<=5&&tier>=2){
@@ -140,7 +140,7 @@ function buyMK(tier) {
 		user["mk"+tier].base += 1
 		if(user["mk"+tier].base > 30 && user["mk"+tier].base % 10 === 0) {
 			user["mk"+tier].costMult *= 1.01;
-			if (user["mk"+tier].base %50 === 0) user["mk"+tier].costMult *= 1.02
+			if (user["mk"+tier].base %50 === 0 && user["mk"+tier].base > 300) user["mk"+tier].costMult *= 1.02
 		}
 	} else if (gravCost.lte(user.gravicles) && user["mk"+(tier-1)].amount.gte(tierCost)&&tier>=6){
 		if (user["mk"+tier].unlocked == true){
@@ -153,7 +153,7 @@ function buyMK(tier) {
 			user["mk"+tier].base += 1
 			if(user["mk"+tier].base > 30 && user["mk"+tier].base % 10 === 0) {
 				user["mk"+tier].costMult *= 1.01;
-				if (user["mk"+tier].base%50 === 0) user["mk"+tier].costMult *= 1.02
+				if (user["mk"+tier].base%50 === 0 && user["mk"+tier].base > 300) user["mk"+tier].costMult *= 1.02
 			}
 		}
 	}
