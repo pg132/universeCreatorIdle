4279195a0af1b7d2516f224b8c84f6b70a3f6e6b
pg132 <42481851+pg132@users.noreply.github.com>
2018-11-13 08:10:18 -0800

    Update main.js

--- main.js
+++ main.js
@@ -154,7 +154,7 @@ function buyMK(tier) {
 			user["mk"+tier].amount = user["mk"+tier].amount.plus(1)
 			user["mk"+tier].base += 1
 			if(user["mk"+tier].base > 30 && user["mk"+tier].base % 10 === 0) {
-				user["mk"+tier].costMult *= 1.01;
+				user["mk"+tier].costMult *= 1.02;
 				if (user["mk"+tier].base%50 === 0 && user["mk"+tier].base > 300) user["mk"+tier].costMult *= w
 			}
 		}
