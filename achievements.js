b349a660a6eeb4618fbe06aad65a766b0738a3b9
pg132 <42481851+pg132@users.noreply.github.com>
2018-11-15 20:48:34 -0800

    Update achievements.js

--- achievements.js
+++ achievements.js
@@ -1,1 +1,16 @@
-
+var achievements = []
+var achieveNames = []
+var possAchieve = []
+function hasAch(ID){
+        return achievements.includes(ID)
+}
+function giveAch(ID){
+        if (possAchieve.includes(ID) && !achievements.includes(ID)) achievements.push(ID)
+}
+function getAllAch(){
+        return achievements       
+}
+function getAchName(ID){
+        var k = 0
+        for (var i = 0; i<possAchieve.length; i++
+}
