ead6c4b2cdde2b314a1b6ad3b610a1ce0880ef2d
pg132 <42481851+pg132@users.noreply.github.com>
2018-11-16 10:34:21 -0800

    Create versions.js

---
+++ versions.js
@@ -0,0 +1,18 @@
+if (version < 0.01){
+        version = 0.01
+        user.points = {
+                amount : user.points.amount
+                upgradesCost:   [     1,     1,     2,     5,    50,    60,    70,    80,    90,   100,   110,   120,   130,    10,    10,    15,    20,   200,   30,    75,    100,    500,    500,   200,    25,   50],//next line GP starts for gravity points and GPA stands for gravity points autobuyer             
+		possibleUpgrade:["GP11","GP12","GP21","GP31","GPA1","GPA2","GPA3","GPA4","GPA5","GPA6","GPA7","GPA8","GPA9","GP41","GP42","GP51","GP52","GPWA","GP61","GP71","GP72","GP10","GP11","GP81","GP91","GP92"],       
+                upgrades: user.points.upgrades ? user.points.upgrades : []
+		autobuyers: user.points.autobuyers ? user.points.autobuyers : [],
+		autobuyerTimes: user.points.autobuyerTimes ? user.points.autobuyerTimes : [10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000],
+		lastTimes: user.points.lastTimes ? user.points.lastTimes : [new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime()]//11 of them
+        }
+}
+
+
+
+
+
+
