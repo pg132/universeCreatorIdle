ba432bbd3ecf106d539e43138788c17ec70bce5f
pg132 <42481851+pg132@users.noreply.github.com>
2018-11-17 15:44:22 -0800

    Update achievements.js

--- achievements.js
+++ achievements.js
@@ -1,5 +1,5 @@
 var achievements = []
-var achieveNames = ["Just Starting","Baby’s first tier upgrade","That’s a lot of gravicles already!","4 score and 4 years ago, subject number 4 was admitted to ","To what? Good question.","This better be worth it","Do you know what you get when you add a 6 and a flipped 6!?!?","Lucky number 6… no that's not right","Last mk achieved!","There’s NINE?",
+var achieveNames = ["Just Starting","Baby’s first tier upgrade","That’s a lot of gravicles already!","4 score and 4 years ago, subject number 4 was admitted to ","To what? Good question.","This better be worth it","Wow! Extra generators!","Lucky number 6… no that's not right","Last mk achieved!","There’s NINE?",
                    "Stupid well, WORK","This really better be worth it","Going above and beyond","Now they’re working better","Sacrifice SACRIFICE SACRIFICE","Not the best idea","Ya see, I pulled a sneaky on ya","See? It is real","That’s a lot of killing","Look ma, no hands!"
                    ]
 var possAchieve = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]
