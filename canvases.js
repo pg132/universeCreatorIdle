43369a6c3285b9a906466a43f81edb583d8409b7
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-11-18 15:48:27 -0600

    Update canvases.js

--- canvases.js
+++ canvases.js
@@ -1,1 +1,37 @@
+var canvas = document.getElementById("studyTreeCanvas");
 
+function resizeCanvas() {
+	canvas.width = 0;
+	canvas.height = 0;
+	canvas.width = document.body.scrollWidth;
+	canvas.height = document.body.scrollHeight;
+	drawUpgradeTree();
+}
+
+function drawTreeBranch(name1, name2) {
+	var start = document.getElementById(name1).getBoundingClientRect();
+	var end = document.getElementById(name2).getBoundingClientRect();
+	var x1 = start.left + (start.width / 2) + (document.documentElement.scrollLeft || document.body.scrollLeft);
+	var y1 = start.top + (start.height / 2) + (document.documentElement.scrollTop || document.body.scrollTop);
+	var x2 = end.left + (end.width / 2) + (document.documentElement.scrollLeft || document.body.scrollLeft);
+	var y2 = end.top + (end.height / 2) + (document.documentElement.scrollTop || document.body.scrollTop);
+	ctx.lineWidth=15;
+	ctx.beginPath();
+	if(user.points.upgrades.includes(name1) && user.points.upgrades.includes(name2)) {
+		ctx.strokeStyle="#B84B5F";
+	} else {
+		ctx.strokeStyle="#5AC467";
+	}
+	ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+function drawUpgradeTree() {
+	drawTreeBranch("GP11", "GP21");
+	drawTreeBranch("GP12", "GP21");
+	drawTreeBranch("GP21", "GP31");
+	drawTreeBranch("GP31", "GP41");
+	drawTreeBranch("GP31", "GP42");
+	drawTreeBranch("GP41", "GP51");
+	drawTreeBranch("GP42", "GP52");
+}
