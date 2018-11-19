db4e9a8cedc141e77c961d256ea7131025002e3e
Aarex Tiaokhiao <aarextiaokhiao@gmail.com>
2018-11-18 22:17:06 -0500

    Remove stuff

--- main.js
+++ main.js
@@ -91,46 +91,14 @@ function getDefaultSave() {
 			amount:0,
 			multipliers:[]
 		},
-		points:{
-			amount:new Decimal(0),
-			upgradesCost:   [     1,     1,     2,     5,    50,    60,    70,    80,    90,   100,   110,   120,   130,    10,    10,    15,    20,   200,    30,    75,   100,    500,    500,   200,    25,    50],//next line GP starts for gravity points and GPA stands for gravity points autobuyer             
-			possibleUpgrade:["GP11","GP12","GP21","GP31","GPA1","GPA2","GPA3","GPA4","GPA5","GPA6","GPA7","GPA8","GPA9","GP41","GP42","GP51","GP52","GPWA","GP61","GP71","GP72","GPA10","GPA11","GP81","GP91","GP92"],       
-			upgrades:[],
-			autobuyers:[],
-			autobuyerTimes:[10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000],
-			lastTimes:[new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime(),new Date().getTime()]//11 of them
-		},
-		eaters:{
-			GE1:{
-				cost: new Decimal(1e100),
-				scale: new Decimal(1e2),
-				amount: 0
-			},
-			GE2:{
-				cost: new Decimal(1e100),
-				scale: new Decimal(1e2),
-				amount: 0
-			},
-			GE3:{
-				cost: new Decimal(1e100),
-				scale: new Decimal(1e2),
-				amount: 0
-			},
-			GE4:{
-				cost: new Decimal(1e100),
-				scale: new Decimal(1e2),
-				amount: 0
-			}
-		},
 		statistics: {
 			playtime: 0,
-			totalGravicles: new Decimal(0),
-			sacrificed: 0
+			totalGravicles: new Decimal(0)
 		},
 		options: {
 			notation: "Scientific"
 		},
-		version: 0.01,
+		version: 0.02,
 		lastTick: new Date().getTime()
 	};
 }
@@ -140,9 +108,6 @@ function getDefaultSave() {
 var user = getDefaultSave();
 
 function getPulseReward(wells){
-	if (user.points.upgrades.includes("GP11") == false){
-		return 1+Math.sqrt(wells/2000)
-	}
 	return 1+Math.pow(wells/2000,.5)*Math.pow(wells,.1)
 }
 
@@ -150,10 +115,9 @@ function buyMK(tier, quick) {
 	var tierCost = user["mk"+tier].previousTierCost
 	var gravCost = user["mk"+tier].cost
 	var costMult = user["mk"+tier].costMult
-	var mainScale = 1+.01/getEaterReward(4)
+	var mainScale = 1.01
 	var buyingMult = 1.01
-	var w = 1+.1/getEaterReward(4)
-	if (user.points.upgrades.includes("GP61")) w = (w-1)*.75+1
+	var w = 1.1
 	if (tier == 1){
 		if (gravCost.lte(user.gravicles)){
 			user.gravicles = user.gravicles.minus(gravCost)
@@ -165,9 +129,8 @@ function buyMK(tier, quick) {
 			if(user.mk1.base > 30 && user.mk1.base % 10 === 0) {
 				user.mk1.costMult *= mainScale;
 				if (user.mk1.base%50 === 0 && user.mk1.base >= 300) user.mk1.costMult *= w
-				if (user.mk1.base%100 == 0 && user.mk1.base > 400) user.mk1.costMult *= 1.25+1.75/getEaterReward(4)
+				if (user.mk1.base%100 == 0 && user.mk1.base > 400) user.mk1.costMult *= 1.25+1.75
 			}
-			giveAch(10)
 		}
 	} else if (gravCost.lte(user.gravicles) && user["mk"+(tier-1)].amount.gte(tierCost)&&tier<=5&&tier>=2){
 		user.gravicles = user.gravicles.minus(gravCost)
@@ -180,9 +143,8 @@ function buyMK(tier, quick) {
 		if(user["mk"+tier].base > 30 && user["mk"+tier].base % 10 === 0) {
 			user["mk"+tier].costMult *= mainScale;
 			if (user["mk"+tier].base %50 === 0 && user["mk"+tier].base >= 300) user["mk"+tier].costMult *= w
-			if (user["mk"+tier].base %100 == 0 && user["mk"+tier].base > 400) user["mk"+tier].costMult *= 1.25+1.75/getEaterReward(4)
+			if (user["mk"+tier].base %100 == 0 && user["mk"+tier].base > 400) user["mk"+tier].costMult *= 1.25+1.75
 		}
-		giveAch(9+tier)
 	} else if (gravCost.lte(user.gravicles) && user["mk"+(tier-1)].amount.gte(tierCost)&&tier>=6){
 		if (user["mk"+tier].unlocked == true){
 			user.gravicles = user.gravicles.minus(gravCost)
@@ -195,9 +157,8 @@ function buyMK(tier, quick) {
 			if(user["mk"+tier].base > 30 && user["mk"+tier].base % 10 === 0) {
 				user["mk"+tier].costMult *= mainScale;
 				if (user["mk"+tier].base %50 === 0 && user["mk"+tier].base >= 300) user["mk"+tier].costMult *= w
-				if (user["mk"+tier].base %100 == 0 && user["mk"+tier].base > 400) user["mk"+tier].costMult *= 1.25+1.75/getEaterReward(4)
+				if (user["mk"+tier].base %100 == 0 && user["mk"+tier].base > 400) user["mk"+tier].costMult *= 1.25+1.75
 			}
-			giveAch(10+tier)
 		}
 	}
 	//abv is init mult scale after e308
@@ -206,18 +167,15 @@ function buyMK(tier, quick) {
 
 function gravityWell(autobuyer){//autobuyer helps us later to see if the user is doing it
 	//first check is if we can afford it
-	if (user["mk"+user.wells.tiercost].amount.gte(new Decimal(user.wells.cost-.0001))){//otherwise kick us out of this function
+	if (user["mk"+user.wells.tiercost].amount.gte(new Decimal(user.wells.cost-.0001))){
 		resetMK() //we need this function DONE
 		if (!(user.wells.tiercost == 9)){
 			user.wells.tiercost += 1
 		} else{
-			user.wells.cost = 20+(user.wells.amount-3)*user.wells.costScale/getEaterReward(2)//might be changed later by an upgrade
+			user.wells.cost = 20+(user.wells.amount-3)*user.wells.costScale
 		}
 		//now do the boosts if so
 		user.wells.amount += 1
-		giveAch(15)
-		if (user.wells.amount >= 5) giveAch(20)
-		if (user.wells.amount >= user.pulse.cost) giveAch(22)
 	}
 }
 
@@ -225,31 +183,13 @@ function updatePulseCost(){
 	var mult = 2
 	var con = -3
 	var frozenAmt = 1//the amount that eaters cant effect, may be change by upgrades later
-	mult = frozenAmt+(mult-frozenAmt)/getEaterReward(1)
-	if (user.points.upgrades.includes("GP21")) con += -3
+	mult = frozenAmt+(mult-frozenAmt)
 	user.pulse.cost = Math.ceil(user.wells.defaultMults*mult+con)
-}
-
-function buyGE(number,amt=1){
-	var k = user.eaters["GE"+number].cost.times(Decimal.pow(user.eaters["GE"+number].scale, amt-1))
-	if (user.gravicles.gte(k)){
-		user.gravicles = user.gravicles.minus(k)
-		user.eaters["GE"+number].cost = k.times(Decimal.pow(user.eaters["GE"+number].scale, amt))
-		user.eaters["GE"+number].amount += amt
-	}
-	
-}
-
-function buyMaxGE(number){
-	while (user.eaters["GE"+number].cost < user.gravicles){
-		buyGE(number)
-	}
 }
 
 function gravityPulse(autobuyer){
 	//first check if we afford
 	if (user.wells.amount >= user.pulse.cost){
-		giveAch(21)
 		//clear mk and then give boosts
 		user.pulse.multipliers.push(getPulseReward(user.wells.amount))//add the thing to the end
 		user = {//update user
@@ -338,22 +278,13 @@ function gravityPulse(autobuyer){
 				costScale:20
 			},
 			pulse:user.pulse,
-			points:user.points,
 			statistics:user.statistics,
-			eaters:user.eaters,
 			options:user.options,
 			version:user.version,
 			lastTick:user.lastTick
 		}
 		user.pulse.amount += 1 //give another pulse
-		if (user.points.upgrades.includes("GP42")) user.wells.amount += 1
-		if (user.points.upgrades.includes("GP91")) {
-			user.gravicles = user.gravicles.plus(1e5)
-			user.mk1.amount = user.mk1.amount.plus(200)
-		}
-		if (user.wells.defaultMults>= 10) giveAch(23)
 		updatePulseCost()
-		if (user.wells.amount >= user.pulse.cost) giveAch(26)
 	}
 }
 
@@ -441,76 +372,28 @@ function resetMK(){
 		},
 		wells:user.wells,
 		pulse:user.pulse,
-		points:user.points,
-		eaters:user.eaters,
 		statistics:user.statistics,
 		options:user.options,
 		version:user.version,
 		lastTick:user.lastTick
 	}
-	if (user.points.upgrades.includes("GP91")) {
-		user.gravicles = user.gravicles.plus(1e5)
-		user.mk1.amount = user.mk1.amount.plus(200)
-	}
-}
-
-function getEaterReward(number){
-	var k = user.eaters["GE"+number].amount
-	var amt = 0.01
-	if (user.points.upgrades.includes("GP72")) amt = amt * 2
-	var comp = false
-	if (user.points.upgrades.includes("GP81")) comp = true
-	if (comp) return Decimal.pow(1+amt,k)
-	return 1+amt*k
-}
-
-function buyGPupg(ID){//ID is a string
-	var k = "GP"+ID
-	if (!user.points.upgrades.includes(k) && user.points.possibleUpgrade.includes(k)){
-		var w = user.points.possibleUpgrade.indexOf(k)
-		 
-		if (w==-1) return 
-		var cost = user.points.upgradesCost[w]
-		if (user.points.amount.gte(cost) && isGPupgradePossible(k)){//buy upgrade then
-			user.points.upgrades.push(k)
-			user.points.amount = user.points.amount.minus(cost)
-		}
-	}
-	resizeCanvas();
-}
-
-function isGPupgradePossible(id) {
-	if (id == "GPA1") return user.points.upgrades.includes("GP31")
-	if (String(id).length >= 4){
-		if (id.substring(0, 3) == "GPA") return user.points.upgrades.includes("GPA"+(parseInt(id.substring(3))-1))
-	}
-	rowid = parseInt(String(id).substring(2))
-	rowid = rowid - rowid%10
-	if (rowid < 20) return true
-	return user.points.upgrades.includes("GP"+(rowid-9)) || user.points.upgrades.includes("GP"+(rowid-8))
 }
 
 function gravityWellBoost(tier){
        	var w = user.wells.amount
 	var d = user.wells.defaultMults
 	var base = 2
-	if (user.points.upgrades.includes("GP51")) base = 2.5
-	base = base*getEaterReward(3)
-	var q = new Decimal(1)
-	if (user.points.upgrades.includes("GP71"))  q = Decimal.pow(2.5,Math.floor(w-tier/5)).max(1)
-	if (w<=d-1+tier) return Decimal.max(1,base**(w-tier+1)).times(q)//fifth is worse
-	return Decimal.pow(base,(d-1)).times(w-d-tier+3).times(q)//just try it, it should work
+	base = base
+	if (w<=d-1+tier) return Decimal.max(1,base**(w-tier+1))//fifth is worse
+	return Decimal.pow(base,(d-1)).times(w-d-tier+3)//just try it, it should work
 }
 
 function fullPowerWellsUpdate(){
 	var constant = 4
-	var multiplier = 1
-	if (user.points.upgrades.includes("GP31")) multiplier = 1.5
 	for (var i = 0; i< user.pulse.amount; i++){
-		constant = Math.ceil(constant*(1+(multiplier*(user.pulse.multipliers[i]-1))))
+		constant = Math.ceil(constant*(1+(user.pulse.multipliers[i]-1)))
 	}
 	user.wells.defaultMults = constant
-	
 }
 
 
@@ -552,45 +435,6 @@ function maxAll(){
 	}
 }
 
-function runMKAutobuyers(){
-	var k = user.points.autobuyers
-	for(var i = 0; i < k.length; i++){
-		giveAch(29)
-		var p = k[i]
-		var number = parseInt(p.substring(2),10)
-		var lastTime = user.points.lastTimes[number]
-		var time = new Date().getTime()
-		var interval = user.points.autobuyerTimes[number]
-		if (interval+lastTime <= time && p<= 9){
-			buyMK(p,true)
-			user.points.lastTimes[number] = time
-		}
-		if (interval+lastTime <= time && !p<= 9){
-			var hi = 0//do other stuff that i will not think abt
-			user.points.lastTimes[number] = time
-		}
-	}
-}
-
-function sacPulses(amt){
-	if (user.pulse.amount>= amt+2 && amt > 0){
-		user.statistics.sacrificed++
-		user.points.amount = user.points.amount.plus(getGPgain(amt)).round()
-		user.pulse.amount -= amt
-		//remove the last amt elems from user.pulse.multipliers this is done by the .pop()
-		for (var i = 0; i<amt;i++){
-			user.pulse.multipliers.pop()
-		}
-		giveAch(24)
-		if (user.pulse.amount == 2) giveAch(25)
-		if (user.points.amount >= 10) giveAch(28)
-	}
-}
-
-function sacMaxPulses(){
-	sacPulses(Math.max(0,user.pulse.amount-2))
-}
-
 function updateMKUnlocks(){
 	var w = user.wells.amount
 	user.mk6.unlocked = false
@@ -602,26 +446,6 @@ function updateMKUnlocks(){
 	if (w >= 3) user.mk8.unlocked = true
 	if (w >= 4) user.mk9.unlocked = true
 }
-
-function checkAchUnlocks(){
-	if (user.pulse.amount == 9 && user.wells.amount == 9 && user.mk9.amount == new Decimal(99)) giveAch(27)
-	
-	
-}
-
-var showPoints = user.pulse.amount >= 6 || user.points.amount.gte(1) || !(user.points.upgrades.length == 0)
-function updateShowPoints(){
-	showPoints = showPoints || user.pulse.amount >= 6 || user.points.amount.gte(1) || !(user.points.upgrades.length == 0)
-}
-function showGravPoints(){
-	document.getElementById("points display").style.display = "none";
-	if (showPoints) document.getElementById("points display").style.display = "";
-}
-function getGPgain(sac) {
-	if (user.points.upgrades.includes("GP52")) return sac*2
-	return sac
-}
-
 
 function showMK(){
 	for(var i = 2; i <= 9; i++) {
@@ -651,22 +475,14 @@ function baseMKproduction(tier){
 	var amt = user["mk"+tier].amount
 	var mult = user["mk"+tier].multiplier
 	mult = mult.times(gravityWellBoost(tier))
-	if (tier == 2 && user.wells.amount >= 1 && user.points.upgrades.includes("GP12")) mult = mult.times(100)
-	if (tier == 9 && (user.points.upgrades.includes("GP41"))) mult = mult.times(1000)
 	mult = mult.times(Decimal.pow(1+1.5/tier,user.wells.defaultMults-4))
 	//put additional mults here
-	if (user.points.upgrades.includes("GP92")) {
-		if (user["mk"+tier].amount == 0) return new Decimal (0)
-		return amt.plus(10).times(mult)
-	}
 	return amt.times(mult)
 }
 
 function baseMKmult(tier){
 	var mult = user["mk"+tier].multiplier
 	mult = mult.times(gravityWellBoost(tier))
-	if (tier == 2 && user.wells.amount >= 1 && user.points.upgrades.includes("GP12")) mult = mult.times(100)
-	if (tier == 9 && (user.points.upgrades.includes("GP41"))) mult = mult.times(1000)
 	mult = mult.times(Decimal.pow(1+1.5/tier,user.wells.defaultMults-4))
 	//put additional mults here
 	return mult
@@ -706,22 +522,9 @@ function buyablePulse() {
 	}
 	return false
 }
-function buyableGE(number){
-	return user.eaters["GE"+number].cost.lte(user.gravicles)
-}
 
 function update(){
 	document.getElementById("gravicle amount").innerHTML = shorten(user.gravicles);
-	if (user.statistics.sacrificed > 0 || user.points.amount.gt(0) || user.points.upgrades.length > 0) {
-		document.getElementById("upgrades button").style.display = "inline"
-	} else {
-		document.getElementById("upgrades button").style.display = "none"
-	}
-	if (user.statistics.totalGravicles.gt(1e100)) {
-		document.getElementById("eaters button").style.display = "inline"
-	} else {
-		document.getElementById("eaters button").style.display = "none"
-	}
 	if (document.getElementById('generators').style.display != "none") {
 		for(var i = 1; i <=9; i++) {
 			var str = "mk"+i+"Amount";
@@ -741,13 +544,6 @@ function update(){
 			}
 		}
 		showMK();
-		showGravPoints();
-		document.getElementById("pointsBuy1").innerHTML = "Sacrifice one galaxy pulse, for one galaxy point";
-		if (buyablePoints()) {
-			document.getElementById("pointsBuy1").className = "button"
-		} else{
-			document.getElementById("pointsBuy1").className = "buttonlocked";
-		}
 		document.getElementById("well").innerHTML = "Reset the game for a boost<br/>Cost: "+Math.ceil(user.wells.cost)+" mk"+Math.ceil(user.wells.tiercost)+"s";
 		document.getElementById("pulse").innerHTML = "Lose all of your previous progress, but get an improvement to wells<br/>Requires: "+user.pulse.cost+" wells";
 		if(buyableWell()) {
@@ -762,41 +558,11 @@ function update(){
 		}
 		document.getElementById("well number").innerHTML = "Gravity Wells: "+user.wells.amount;
 		document.getElementById("pulse number").innerHTML = "Gravitational Pulses: "+user.pulse.amount+" ("+user.wells.defaultMults+" wells at full power)";
-		document.getElementById("point amount").innerHTML = "Gravitational Points: "+shorten(user.points.amount);
-	}
-	if (document.getElementById('upgrades').style.display != "none") {
-		document.getElementById("point amount upgrades").innerHTML = "You have " + shorten(user.points.amount) + " Gravitational Points."
-		for (var i=0;i<user.points.possibleUpgrade.length;i++) {
-			var upgid = user.points.possibleUpgrade[i]
-			var div = document.getElementById(upgid)
-			if (div !== null) {
-				if (user.points.upgrades.includes(upgid)) div.className = "upgradebtn buttonbought"
-				else {
-					if (user.points.amount.lt(user.points.upgradesCost[i]) || !isGPupgradePossible(upgid)) div.className = "upgradebtn buttonlocked"
-					else div.className = "upgradebtn button"
-				}
-			}
-		}
-		document.getElementById("GP41 effect").innerHTML = shorten(1e3)
-	}
-	if (document.getElementById('eaters').style.display){
-		if (document.getElementById('eaters').style.display != "none") {
-			for (var i = 1; i<= 4; i++){
-				document.getElementById("eater"+i).innerHTML = "Upgrade  Gravity Eater #" + i + "<br>" + (["Pulses are cheaper", "Wells are cheaper", "Wells are stronger", "MK Scalings are decreased"])[i-1] + " by " + shorten(user.eaters["GE" + i].amount) + "%<br>Cost: " + shorten(user.eaters["GE"+i].cost)
-				if (buyableGE(i)) document.getElementById("eater"+i).className = "upgradebtn button"
-				else document.getElementById("eater"+i).className = "upgradebtn buttonlocked"
-			}
-		}
 	}
 	if (document.getElementById('statistics').style.display != "none") {
 		document.getElementById("playtime").innerHTML = "You have played this game for " + timeDisplay(user.statistics.playtime) + "."
 		document.getElementById("total gravicles").innerHTML = "You made " + shorten(user.statistics.totalGravicles) + " gravicles in total."
-		var showMoreStats = user.statistics.sacrificed > 0
-		if (showMoreStats) {
-			document.getElementById("sacrifices stat").style.display = "block"
-			document.getElementById("sacrifices stat").innerHTML = "You have sacrificed pulses " + user.statistics.sacrificed + " time" + (user.statistics.sacrificed == 1 ? "." : "s.") 
-		} else document.getElementById("sacrifices stat").style.display = "none"
-		showMoreStats = showMoreStats || user.pulse.amount > 0
+		var showMoreStats = showMoreStats || user.pulse.amount > 0
 		if (showMoreStats) {
 			document.getElementById("pulses stat").style.display = "block"
 			document.getElementById("pulses stat").innerHTML = "You currently have " + user.pulse.amount + " pulse" + (user.pulse.amount == 1 ? "." : "s.")
@@ -893,9 +659,6 @@ function gameLoop(){
 	updateMKUnlocks();
 	fullPowerWellsUpdate();
 	updatePulseCost();
-	updateShowPoints();
-	runMKAutobuyers();
-	checkAchUnlocks();
 	update();
 }
 
@@ -920,5 +683,4 @@ function startInterval(){
 	load();
 	setInterval(gameLoop,33);
 	setInterval(save,5000);
-	window.addEventListener("resize", resizeCanvas);
 }
