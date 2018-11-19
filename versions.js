81a8ab230bf9239b99c0fea372dd25b862322a49
pg132 <42481851+pg132@users.noreply.github.com>
2018-11-19 10:27:58 -0800

    Update versions.js

--- versions.js
+++ versions.js
@@ -7,8 +7,12 @@ function updateSave() {
 	if (user.version < 0.1) {
 		user.version = 0.1
 		if (user.points === undefined) {
+			var k = user.points
+			if (k) k = user.points.amount // if user.points is a thing
+			else k = new Decimal (0) // if user.points isnt a thing, then set to 0
+			if (!k) k = new Decimal (0) // if user.points is a thing but user.points.amount isnt
 			user.points = {
-				amount: user.points.amount ? user.points.amount: new Decimal(0),
+				amount: k, // then set the amount to what we want it to be
 				upgradesCost:   [     1,     1,     2,     5,    50,    60,    70,    80,    90,   100,   110,   120,   130,    10,    10,    15,    20,   200,    30,    75,   100,    500,    500,   200,    25,    50],//next line GP starts for gravity points and GPA stands for gravity points autobuyer             
 				possibleUpgrade:["GP11","GP12","GP21","GP31","GPA1","GPA2","GPA3","GPA4","GPA5","GPA6","GPA7","GPA8","GPA9","GP41","GP42","GP51","GP52","GPWA","GP61","GP71","GP72","GPA10","GPA11","GP81","GP91","GP92"],       
 				upgrades:[],
