5a6f475f745f3b97f257ee8300f6e7134ff7784b
pg132 <42481851+pg132@users.noreply.github.com>
2018-11-19 10:14:18 -0800

    Update versions.js

--- versions.js
+++ versions.js
@@ -6,10 +6,9 @@ function updateSave() {
 	}
 	if (user.version < 0.1) {
 		user.version = 0.1
-		user.points.amount = Decimal.round(user.points.amount)
 		if (user.points === undefined) {
 			user.points = {
-				amount:new Decimal(0),
+				amount: user.points.amount ? user.points.amount: new Decimal(0),
 				upgradesCost:   [     1,     1,     2,     5,    50,    60,    70,    80,    90,   100,   110,   120,   130,    10,    10,    15,    20,   200,    30,    75,   100,    500,    500,   200,    25,    50],//next line GP starts for gravity points and GPA stands for gravity points autobuyer             
 				possibleUpgrade:["GP11","GP12","GP21","GP31","GPA1","GPA2","GPA3","GPA4","GPA5","GPA6","GPA7","GPA8","GPA9","GP41","GP42","GP51","GP52","GPWA","GP61","GP71","GP72","GPA10","GPA11","GP81","GP91","GP92"],       
 				upgrades:[],
@@ -39,6 +38,6 @@ function updateSave() {
 					amount: 0
 				}
 			}
-		}
+		} 
 	}
 }
