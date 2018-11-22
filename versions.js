8e35dbb94647821d4a1533327e26368904ce021b
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-11-21 21:31:10 -0600

    Update versions.js

--- versions.js
+++ versions.js
@@ -26,8 +26,9 @@ function updateSave() {
 			if (!k) k = new Decimal (0) // if user.points is a thing but user.points.amount isnt
 			user.points = {
 				amount: k, // then set the amount to what we want it to be
-				upgradesCost:   [     1,     1,     2,     5,    50,    60,    70,    80,    90,   100,   110,   120,   130,    10,    10,    15,    20,   200,    30,    75,   100,    500,    500,   200,    25,    50],//next line GP starts for gravity points and GPA stands for gravity points autobuyer             
-				possibleUpgrade:["GP11","GP12","GP21","GP31","GPA1","GPA2","GPA3","GPA4","GPA5","GPA6","GPA7","GPA8","GPA9","GP41","GP42","GP51","GP52","GPWA","GP61","GP71","GP72","GPA10","GPA11","GP81","GP91","GP92"],       
+				upgradesCost:   [     1,     1,          2,     5,    50,    60,    70,    80,    90,   100,   110,   120,   130,    10,    10,    15,    20,    200,         30,    75,   100,    500,        200,    25,    50],//next line GP starts for gravity points and GPA stands for gravity points autobuyer             
+				possibleUpgrade:["GP11","GP12",     "GP21","GP31","GPA1","GPA2","GPA3","GPA4","GPA5","GPA6","GPA7","GPA8","GPA9","GP41","GP42","GP51","GP52","GPA10",     "GP61","GP71","GP72","GPA11",     "GP81","GP91","GP92"],       
+				requirements:   [    "",    "","GP11,GP12","GP21","GP31","GPA1","GPA2","GPA3","GPA4","GPA5","GPA6","GPA7","GPA8","GP31","GP31","GP41","GP42", "GPA9","GP51,GP52","GP61","GP61","GPA10","GP71,GP72","GP81","GP81"],       
 				upgrades:[],
 				autobuyers:[],
 				autobuyerTimes:[10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000],
